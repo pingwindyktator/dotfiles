d852ca1b5cd53f975be97c08bade928a66c3dbfd
pingwindyktator <ja2222ja@gmail.com>
2018-09-22 13:17:52 +0200

    refactor install script

--- install.sh
+++ install.sh
@@ -2,107 +2,89 @@
 
 platform=""
 backup_dir="$HOME/dotfilesbackup"
+update_dir="$HOME/.dotfiles"
+
+confirm() {
+    read -r -p "${1} [y/N] " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+generic_install() {
+    cd ${update_dir}/${platform} > /dev/null
+    find . ! -path . -and ! -path './.git/*' -and ! -path './.git' -type d | xargs -i mkdir -p ${HOME}/{}
+    find -type f | xargs -i cp {} ${HOME}/{}
+}
 
 install_unix() {
     sudo apt-get install git vim -y
     sudo apt-get install xdotool wmctrl colordiff -y
-    base_url="https://raw.githubusercontent.com/pingwindyktator/dotfiles/master/unix/"
-    mkdir -p "$HOME/bin"
-
-    for dotfilename in ".bash_aliases" ".bash_funcs" ".bash_profile" ".bashrc" ".gitconfig" ".vimrc" "bin/init_git_sign_repo" "bin/terminal_quick_access"; do
-        wget -q "$base_url/$dotfilename" -O "$HOME/$dotfilename"
-    done
-    
-    chmod u+x "$HOME/bin/init_git_sign_repo"
-    chmod u+x "$HOME/bin/terminal_quick_access"
+    generic_install
 }
 
 install_bash_for_windows() {
     sudo apt-get install git vim colordiff -y
-    base_url="https://raw.githubusercontent.com/pingwindyktator/dotfiles/master/bash_for_windows/"
-    mkdir -p "$HOME/bin"
-
-    for dotfilename in ".bash_aliases" ".bash_funcs" ".bash_profile" ".bashrc" ".gitconfig" ".vimrc" ".inputrc" "bin/init_git_sign_repo"; do
-        wget -q "$base_url/$dotfilename" -O "$HOME/$dotfilename"
-    done
-    
-    chmod u+x "$HOME/bin/init_git_sign_repo"
+    generic_install
 }
 
 install_cygwin() {
-    base_url="https://raw.githubusercontent.com/pingwindyktator/dotfiles/master/cygwin"
-
-    for dotfilename in ".bash_aliases" ".bash_funcs" ".bashrc" ".gitconfig" ".inputrc" ".vimrc" ".vs_for_bash"; do
-        wget -q "$base_url/$dotfilename" -O "$HOME/$dotfilename"
-    done
-}
-
-install_macos() {
-    echo "Mac OS is currently not supported"
-}
-
-install_win() {
-    echo "Windows is currently not supported"
+    generic_install
 }
 
 backup_existing_files() {
-    mkdir -p "$backup_dir"
-    for dotfilename in ".bash_aliases" ".bash_funcs" ".bash_profile" ".bashrc" ".gitconfig" ".inputrc" ".vimrc" ".vs_for_bash" "bin/init_git_sign_repo" "bin/terminal_quick_access"; do
-        if [ -f "$HOME/$dotfilename" ]; then
-            echo "backuping $dotfilename to $backup_dir..."
-            mv "$HOME/$dotfilename" "$backup_dir/$dotfilename"
-        fi
-    done
+    mkdir -p ${backup_dir}
+    cd ${update_dir}/${platform} > /dev/null
+    find . ! -path . -and ! -path './.git/*' -and ! -path './.git' -type d | xargs -i mkdir -p ${backup_dir}/{}
+    find -type f | xargs -i cp ${HOME}/{} ${backup_dir}/{} 2> /dev/null
 }
 
 preinstall() {
-    echo ""
+    mkdir -p ${update_dir}
+    cd ${update_dir} > /dev/null
+    git init -q
+    git remote add -f origin https://github.com/pingwindyktator/dotfiles > /dev/null
+    git config core.sparseCheckout true
+    echo ${platform} >> .git/info/sparse-checkout
+    git pull -q origin master
+    cd ${platform}
+
+    read -r -p "Enter git user.name: " response
+    find -type f | xargs -i sed -i "s/##git_name##/${response}/g" {}
+    read -r -p "Enter git user.email: " response
+    find -type f | xargs -i sed -i "s/##git_email##/${response}/g" {}
+    read -r -p "Enter git user.signingKey: " response
+    find -type f | xargs -i sed -i "s/##git_signingKey##/${response}/g" {}
 }
 
 postinstall() {
-    source "$HOME/.bashrc"
+    rm -rf ${update_dir}
 }
 
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    platform="macos"
-    func=install_macos
 
-elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" && "$(uname -a)" == *"Microsoft"* ]]; then
+
+if [[ "$(expr substr $(uname -s) 1 5)" == "Linux" && "$(uname -a)" == *"Microsoft"* ]]; then
     platform="bash_for_windows"
-    func=install_bash_for_windows
 
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     platform="unix"
-    func=install_unix
-
-elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]]; then
-    platform="win32"
-    func=install_win
-
-elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]; then
-    platform="win64"
-    func=install_win
 
 elif [[ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]]; then
     platform="cygwin"
-    func=install_cygwin
 
 else
     echo "unknown platform"
     exit 1
 fi
 
-read -p "install dotfiles of $platform platform? [Y/n] "
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    exit 0
-fi
-
-read -p "backup existing files to $backup_dir? [Y/n] "
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    backup_existing_files
-fi
-
+confirm "Install dotfiles of $platform platform?" || exit 0
 preinstall
-$func
+confirm "Backup existing files to $backup_dir?" && backup_existing_files
+install_${platform}
 postinstall
